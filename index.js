41a21fb6added76b23aa158707191915563081bf
s22007 <s22007>
2022-12-23 10:05:09 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+export default function Home () {
+  return (
+    <>
+      <header>HEADER</header>
+
+      <main>
+        <div>
+          <h1>CUBE</h1>
+          <p>アウトプットしてくサイト</p>
+        </div>
+      </main>
+
+      <footer>FOOTER</footer>
+    </>
+  )
+}
