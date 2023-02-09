5739f99465c89018e9913b6ab03dd463d08afba9
s22007 <s22007>
2023-02-09 08:02:41 +0000

    chap9

---
+++ components/posts.js
@@ -0,0 +1,18 @@
+import styles from 'styles/posts.module.css'
+import Link from 'next/link'
+
+export default function Posts ({ posts }) {
+  return (
+    <div className={styles.gridContainer} legacyBehavior>
+      {posts.map(({ title, slug }) => (
+        <article className={styles.post} key={slug}>
+          <Link href={`/blog/${slug}`} legacyBehavior>
+            <a>
+              <h2>{title}</h2>
+            </a>
+          </Link>
+        </article>
+      ))}
+    </div>
+  )
+}
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,7 +1,10 @@
+import { getAllPosts } from 'lib/api'
 import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
-export default function Blog () {
+import Posts from 'components/posts'
+
+export default function Blog ({ posts }) {
   return (
     <Container>
       <Meta pageTitle='ブログ' pageDesc='ブログの記事一覧' />
@@ -9,6 +12,16 @@ export default function Blog () {
         title='Blog'
         subtitle='Recent Posts'
       />
+      <Posts posts={posts} />
     </Container>
   )
+}
+export async function getStaticProps () {
+  const posts = await getAllPosts()
+
+  return {
+    props: {
+      posts: posts
+    }
+  }
 }
